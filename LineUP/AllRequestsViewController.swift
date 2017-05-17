36baea6038cf1f3e429b90b89b26934b931c2c8f
Archie <Archie@Arkadijss-MacBook-Pro.local>
2017-05-17 19:23:11 +0800

    layout

---
+++ LineUP/AllRequestsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AllRequestsViewController.swift
+//  LineUP
+//
+//  Created by Arkadijs Makarenko on 17/05/2017.
+//  Copyright © 2017 ArchieApps. All rights reserved.
+//
+
+import UIKit
+
+class AllRequestsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
