944b7c65adbb03f1cdf35aaeef2c678039ca1c7c
ardMac <adliramli88@gmail.com>
2017-05-17 15:22:24 +0800

    adding outlets to FirstViewController

--- LineUP/FirstViewController.swift
+++ LineUP/FirstViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var positionLabel: UILabel!
+    @IBOutlet weak var departmentLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var supervisorLabel: UILabel!
+    @IBOutlet weak var annualLeaveLabel: UILabel!
+    @IBOutlet weak var maternityLeaveLabel: UILabel!
+    @IBOutlet weak var paternityLeaveLabel: UILabel!
+    @IBOutlet weak var emergencyLeaveLabel: UILabel!
+    @IBOutlet weak var studyLeaveLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
