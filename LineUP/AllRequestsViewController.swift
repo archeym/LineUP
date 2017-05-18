d08a30270523522b6f9fdaf920e3ebaa840593e7
ardMac <adliramli88@gmail.com>
2017-05-18 20:58:47 +0800

    adding RequestedTableViewCell swift and xib

--- LineUP/AllRequestsViewController.swift
+++ LineUP/AllRequestsViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class AllRequestsViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!{
+        didSet{
+            tableView.dataSource = self
+            tableView.delegate = self
+            tableView.register(RequestedTableViewCell.cellNib, forCellReuseIdentifier: RequestedTableViewCell.cellIdentifier)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+}
+
+extension AllRequestsViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 148
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RequestedTableViewCell.cellIdentifier) as? RequestedTableViewCell
+            else {return UITableViewCell()}
+        return cell
     }
 }
