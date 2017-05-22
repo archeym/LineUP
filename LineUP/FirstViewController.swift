e9349725007ff28afaeb23401c218cac95e9404e
ardMac <adliramli88@gmail.com>
2017-05-22 20:53:27 +0800

    getting profile photo from api

--- LineUP/FirstViewController.swift
+++ LineUP/FirstViewController.swift
@@ -130,7 +130,44 @@ class FirstViewController: UIViewController {
         self.studyLeaveLabel.text = String(self.currentUser.studyLeave)
         self.nonpaidLeaveLabel.text = String(self.currentUser.nonPaid)
         //self.profileImageView.image = UIImage(self.currentUser.profilePhoto)
+        self.profileImageView.loadImageUsingCacheWithUrlString(urlString: self.currentUser.profilePhoto)
         
     }
 }
 
+let imageCache = NSCache<AnyObject, AnyObject>()
+
+extension UIImageView {
+    
+    func loadImageUsingCacheWithUrlString(urlString: String) {
+        
+        self.image = nil
+        
+        // Check cache for image first
+        if let cachedImage = imageCache.object(forKey: urlString as NSString) {
+            self.image = cachedImage as? UIImage
+            return
+        }
+        
+        // Otherwise fire off a new download
+        let url = NSURL(string: urlString)
+        URLSession.shared.dataTask(with: url! as URL, completionHandler: { (data, response, error) in
+            
+            // Dowload hit an error so let's return out
+            if error != nil {
+                print(error!)
+                return
+            }
+            DispatchQueue.main.async(execute: {
+                
+                if let downloadedImage = UIImage(data: data!) {
+                    imageCache.setObject(downloadedImage, forKey: urlString as NSString)
+                    self.image = downloadedImage
+                }
+            })
+        }).resume()
+    }
+    
+    
+    
+}
